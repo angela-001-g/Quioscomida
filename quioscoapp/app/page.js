d3540431fe3b23775f76cfa320ea073ad4c7fa59
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-15 17:46:36 -0500

    ts

--- quioscoapp/app/page.js
+++ quioscoapp/app/page.js
@@ -1,7 +1,27 @@
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({categorias}) {
+
+ console.log(categorias)
+
   return (
     <h1>App Next</h1>
   )
 }
+
+export const getServerSideProps = async() => {
+
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+
+  return {
+    props:{
+      categorias
+    }
+  }
+
+}
+
+
