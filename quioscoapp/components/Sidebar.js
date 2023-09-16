f7af22beac4fdbd3b1e69f481a66a493ab4af990
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-15 19:13:16 -0500

    imagenes

---
+++ quioscoapp/components/Sidebar.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+function Sidebar() {
+  return (
+    <>
+        <Image width={300} height={100} src="/assets/img/logo.svg" alt="imagen logotipo"></Image>
+    </>
+  )
+}
+
+export default Sidebar
