2b1da9c4b976d4ed1b0a8cf12005d5edde9a2ca1
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-20 14:25:48 -0500

    mostrando los productos de la categoria actual

--- quioscoapp/app/page.js
+++ quioscoapp/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React, { use  } from "react";
 import Layout from "../layout/Layout"
 import useQuiosco from '../hooks/useQuiosco';
-
+import Producto from '../components/Producto'
 
 
 export default function Home() {
@@ -13,10 +13,21 @@ const { categoriaActual } = useQuiosco()
 
   return (
     <Layout  >
+
       <h1 className='text-4xl font-black' > {categoriaActual?.nombre} </h1>
       <p className='text-2xl my-10' >
         Elige y personaliza tu pedido a continuación 
       </p>
+
+      <div className='grid gap-4 grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4' >
+      {categoriaActual?.productos?.map( producto => (
+        <Producto
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
+      </div>
+
     </Layout>
   )
 }
