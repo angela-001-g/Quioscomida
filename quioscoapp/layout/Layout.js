17b109aa8dc19f7a3c87bf83be8e1417a9b3d12c
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-21 19:14:03 -0500

    modal

--- quioscoapp/layout/Layout.js
+++ quioscoapp/layout/Layout.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar"
+import Modal from 'react-modal'
+import useQuiosco from "../hooks/useQuiosco";
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
 
 export default function Layout({children, pagina}){
+
+    const { modal } = useQuiosco()
+
     return(
         <>
             <Head>
@@ -19,6 +36,17 @@ export default function Layout({children, pagina}){
                     </div>
                 </main>    
             </div> 
+
+            
+                <Modal
+                    isOpen={modal}
+                    style={customStyles}
+
+                >
+                    <h1>Modal...</h1>
+                </Modal>
+            
+
         </>
     )
 }
