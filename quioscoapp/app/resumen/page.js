66bd8c360810a629a4c69c37b1ac68c55a137882
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-22 16:06:28 -0500

    navegación completada

--- quioscoapp/app/resumen/page.js
+++ quioscoapp/app/resumen/page.js
@@ -1,8 +1,14 @@
 'use client'
 
+import Layout from "../../layout/Layout"
+
 export default function Resumen() {
     return(
     <>
-        <h1>Resumen</h1>
+        <Layout >
+            <h1 className="text-4xl font-black" >Resumen</h1>
+            <p className="text-2xl my-10" >Revisa tu pedido</p>
+        </Layout>
     </>
-)}
+    )
+}
--- quioscoapp/app/total/page.js
+++ quioscoapp/app/total/page.js
@@ -1,8 +1,14 @@
 'use client'
 
+import Layout from "../../layout/Layout"
+
+
 export default function Total() {
     return(
-    <>
-        <h1>Total</h1>
+        <>
+        <Layout >
+            <h1 className="text-4xl font-black" >Total y Confirmar pedido</h1>
+            <p className="text-2xl my-10" >Confirma tu pedido a continuación</p>
+        </Layout>
     </>
 )}
