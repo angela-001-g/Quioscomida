39a640dbc0de5d44b1076233953203e10ea7d566
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-26 22:32:39 -0500

    resumen producto

--- quioscoapp/app/resumen/page.js
+++ quioscoapp/app/resumen/page.js
@@ -1,13 +1,29 @@
 'use client'
 
 import Layout from "../../layout/Layout"
+import useQuiosco from "../../hooks/useQuiosco"
+import ResumenProducto from "../../components/ResumenProducto"
 
 export default function Resumen() {
+
+    const { pedido } = useQuiosco
+
     return(
     <>
         <Layout >
             <h1 className="text-4xl font-black" >Resumen</h1>
             <p className="text-2xl my-10" >Revisa tu pedido</p>
+            <ResumenProducto />
+            { pedido?.length === 0 ? (
+                <p className="text-center text-2xl" > No hay elementos en tu pedido</p>
+            ) : (
+                pedido?.map(producto => (
+                <ResumenProducto
+                    key={producto.id}
+                    producto={producto}
+                />
+                ))
+            )}
         </Layout>
     </>
     )
---
+++ quioscoapp/components/ResumenProducto.js
@@ -0,0 +1,11 @@
+'use client'
+
+function ResumenProducto({producto}) {
+  return (
+    <div>
+      {producto?.nombre}
+    </div>
+  )
+}
+
+export default ResumenProducto
