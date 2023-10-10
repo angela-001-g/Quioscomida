9120a24c76f979f94aeefa98fb55f1a2d582f94f
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-10 18:26:05 -0500

    creando endpoint para obtener las ordenes

--- quioscoapp/app/admin/page.js
+++ quioscoapp/app/admin/page.js
@@ -1,4 +1,4 @@
-import AdminLayout from "../../layout/AdminLayout"
+import AdminLayout from '../../layout/adminLayout'
 
 export default function Admin() {
 
--- quioscoapp/app/api/orders/route.js
+++ quioscoapp/app/api/orders/route.js
@@ -1,10 +1,29 @@
 import { NextResponse } from 'next/server'
 import { Prisma, PrismaClient } from '@prisma/client'
 
+export async function GET(){
+
+        const prisma = new PrismaClient()
+        // Obtener ordenes
+        const ordenes = await prisma.orden.findMany({
+          where: {
+            estado: false
+          }
+        })
+
+
+   
+  return NextResponse.json(ordenes)
+}
+
 export async function POST(request){
     const res = await request.json()
     const prisma = new PrismaClient()
     let orden
+
+
+    // Crear ordenes
+
     if(request.method === "POST"){
       orden = await prisma.orden.create({
         data: {
