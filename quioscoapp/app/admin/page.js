6f9d9109e9d66152d161178aedee0f3ad1a6a03d
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-10 19:44:06 -0500

    primeros pasos mostrando las ordenes

--- quioscoapp/app/admin/page.js
+++ quioscoapp/app/admin/page.js
@@ -2,6 +2,7 @@
 import useSWR from 'swr'
 import axios from 'axios'
 import AdminLayout from '../../layout/adminLayout'
+import Orden from '../../components/Orden'
 
 export default function Admin() {
 
@@ -9,14 +10,21 @@ export default function Admin() {
 
     const { data, error, isLoading } = useSWR('/api/orders', fetcher)
 
-    console.log(data)
-    console.log(error)
-    console.log(isLoading)
+    // console.log(data)
+    // console.log(error)
+    // console.log(isLoading)
 
     return (
         <AdminLayout pagina={"Admin"} >
             <h1 className="text-4xl font-black">Panel de administración</h1>
             <p className="text-2xl my-10">Administra las ordenes</p>
+
+            {data && data.length ? 
+                data.map(orden =>
+                <Orden
+                    key={orden.id}
+                    orden={orden}
+                /> ) : <p>No hay ordenes pendientes</p> }
         </AdminLayout>
     )
 }
