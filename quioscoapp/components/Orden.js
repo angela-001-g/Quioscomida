dabd3885efec520a34b723e50cfa58d39defca44
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-10 19:56:22 -0500

    mostrando el resto de la info de las ordenes

--- quioscoapp/components/Orden.js
+++ quioscoapp/components/Orden.js
@@ -1,4 +1,5 @@
-
+import Image from "next/image"
+import { formatearDinero } from "../helpers"
 
 function Orden({orden}) {
     const {id, nombre, total, pedido} = orden
@@ -8,8 +9,30 @@ function Orden({orden}) {
         <p className="text-lg font-bold">Cliente: {nombre}</p>
 
         <div>
-            
+            {pedido.map(platillo =>(
+                <div key={platillo.id} className="py-3 flex border-b last-of-type:border-0 items-center" >
+                    <div className="w-32">
+                        <Image
+                            width={400}
+                            height={500}
+                            src={`/assets/img/${platillo.imagen}.jpg`}
+                            alt={`Imagen Platillo ${platillo.nombre}`}
+                        />
+                    </div>
+                    <div className="p-5 space-y-2">
+                        <h4 className="text-xl font-bold text-amber-500">{platillo.nombre}</h4>
+                        <p className="text-lg font-bold">Cantidad: {platillo.cantidad}</p>
+                    </div>
+                </div>
+            ))}
         </div>
+
+        <div className="md:flex md:items-center md:justify-between my-10">
+            <p className="mt-5 font-blasck text-4xl text-amber-500">
+                Total a pagar: {formatearDinero(total)}
+            </p>
+        </div>
+
     </div>
   )
 }
